b28a337a08ecee7115dcc1985fc8ec44ebe06256
minxng <minxng12@gmail.com>
2025-05-15 20:10:57 +0900

    [feature] 도서 검색 구현

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 import { BsPersonCircle } from "react-icons/bs";
 import logo from "../../public/logo.png";
 
 export default function Navigation() {
+  const [keyword, setKeyword] = useState("");
+  const router = useRouter();
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/list/${keyword}`);
+  };
   return (
     <nav className="flex max-w-[1200px] w-4/5 mx-auto my-0 py-4 gap-10 items-center justify-between">
       <h1>
@@ -14,7 +24,14 @@ export default function Navigation() {
       </h1>
       <div className="border-emerald-800 border-1 p-2 rounded-2xl flex items-center  w-1/2">
         <BiSearch size={24} color="#527555" />
-        <input type="text" className="focus:outline-0 w-full px-2" />
+        <form action="" onSubmit={handleOnSubmit}>
+          <input
+            type="text"
+            value={keyword}
+            onChange={(e) => setKeyword(e.target.value)}
+            className="focus:outline-0 w-full px-2"
+          />
+        </form>
       </div>
       <div>
         <BsPersonCircle size={30} color="#527555" />
