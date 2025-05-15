e9b888f3bad26855d6cb46b8ad854e0f86355c1c
minxng <minxng12@gmail.com>
2025-05-15 12:51:28 +0900

    [setting] img url config 설정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dfd3hx72e/image/upload/v1746592572/**",
+        search: "",
+      },
+    ],
+  },
   /* config options here */
 };
 
