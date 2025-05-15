6228401b9c1c08343b9e4a54f17cfe1b6b11c9f4
minxng <minxng12@gmail.com>
2025-05-15 17:55:49 +0900

    [feature] 메인 페이지 슬라이드 데이터 호출

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,22 +1,30 @@
 import Banner from "@/components/Banner";
 import BookSlide from "@/components/BookSlide";
 
-async function getBooks() {
+async function getBestSeller() {
   const response = await fetch(
-    "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbskmnjng2132001&QueryType=ItemNewAll&MaxResults=10&start=1&SearchTarget=Book&output=JS&Version=20131101"
+    "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbskmnjng2132001&QueryType=Bestseller&MaxResults=10&start=1&SearchTarget=Book&output=JS&Version=20131101"
   );
   const json = await response.json();
-  console.log("?");
+  return json;
+}
+
+async function getNewBooks() {
+  const response = await fetch(
+    "http://www.aladin.co.kr/ttb/api/ItemList.aspx?ttbkey=ttbskmnjng2132001&QueryType=ItemNewSpecial&MaxResults=10&start=1&SearchTarget=Book&output=JS&Version=20131101"
+  );
+  const json = await response.json();
   return json;
 }
 
 export default async function Home() {
-  const books = await getBooks();
+  const bestseller = await getBestSeller();
+  const newBooks = await getNewBooks();
   return (
-    <div className="w-4/5 mx-auto my-0">
+    <div className="w-4/5 max-w-[1200px] mx-auto my-0">
       <Banner />
-      <p>{JSON.stringify(books)}</p>
-      <BookSlide />
+      <BookSlide books={bestseller} title="베스트셀러 TOP 10" />
+      <BookSlide books={newBooks} title="이달의 신간도서" />
     </div>
   );
 }
