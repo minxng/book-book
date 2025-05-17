febac4ffbaa38811ccd71a371f2366d7abc4d1e4
minxng <minxng12@gmail.com>
2025-05-17 20:23:16 +0900

    [feature] 리스트 책 클릭시 상세 페이지 이동

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface BookItem {
   itemId: string;
@@ -10,6 +11,7 @@ interface BookItem {
   pubDate: string;
   publisher: string;
   bestRank: number;
+  isbn13: number;
 }
 interface BooksProps {
   books: {
@@ -22,13 +24,17 @@ export default function List({ books }: BooksProps) {
     <ul className="basis-4/5">
       {books.item.map((book) => (
         <li key={book.itemId} className="flex gap-8 mb-12">
-          <div className="basis-1/5">
-            <Image src={book.cover} alt="표지지" width={200} height={200} />
-          </div>
+          <Link href={`/book/${book.isbn13}`}>
+            <div className="basis-1/5">
+              <Image src={book.cover} alt="표지지" width={200} height={200} />
+            </div>
+          </Link>
           <div className="basis-4/5">
             <p>{book.bestRank}</p>
             <p>{book.categoryName}</p>
-            <p className="text-xl font-bold">{book.title}</p>
+            <Link href={`/book/${book.isbn13}`}>
+              <p className="text-xl font-bold">{book.title}</p>
+            </Link>
             <p>
               {book.author} | {book.publisher} | {book.pubDate}
             </p>
