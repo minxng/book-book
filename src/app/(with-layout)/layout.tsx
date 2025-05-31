46f463a3fba5e0f6901344067a6b154ba94e2f99
minxng <minxng12@gmail.com>
2025-05-31 18:17:05 +0900

    [feature] footer 추가

--- src/app/(with-layout)/layout.tsx
+++ src/app/(with-layout)/layout.tsx
@@ -8,10 +8,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <main className="min-h-screen flex flex-col">
       <Navigation />
       <main className="flex-1">{children}</main>
       <Footer />
-    </>
+    </main>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className=" bg-primary py-10 static bottom-0">
+      <div className="mx-auto w-4/5 max-w-[1200px]">footer~~</div>
+    </footer>
+  );
+}
