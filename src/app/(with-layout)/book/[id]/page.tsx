c877cc12298a5a4f157203dcf34d10acb7e0f98a
minxng <minxng12@gmail.com>
2025-06-25 22:37:31 +0900

    [feature] html 특수기호 디코딩

--- src/app/(with-layout)/book/[id]/page.tsx
+++ src/app/(with-layout)/book/[id]/page.tsx
@@ -1,6 +1,7 @@
 import ReviewButton from "@/components/ReviewButton";
 import WishButton from "@/components/WishButton";
 import { getBookDetail } from "@/lib/api/aladin";
+import he from "he";
 import Image from "next/image";
 
 export default async function BookDetail({
@@ -11,22 +12,27 @@ export default async function BookDetail({
   const { id } = await params;
   const book = await getBookDetail(id);
   return (
-    <section className="container-style mt-8">
-      <h3 className="text-xl mb-4">{book.title}</h3>
-      <div className="flex">
+    <section className="container-style pt-8">
+      <div className="flex gap-10">
         <Image
           src={book.cover.replace("coversum", "cover500")}
           alt="book cover"
           width={300}
           height={500}
         />
-        <div className="flex flex-col w-1/2">
-          <p>{book.description}</p>
+        <div className="flex flex-col w-1/2 gap-2">
+          <h3 className="text-xl">
+            {he.decode(book.title)}{" "}
+            <span className="text-sm ml-2">{book.author}</span>
+          </h3>
+          <p>{he.decode(book.description)}</p>
           <span>{book.subTitle}</span>
-          <span>{book.author}</span>
+
+          <div className="flex gap-2">
+            <WishButton book={book} />
+            <ReviewButton book={{ ...book, id: book.itemId }} />
+          </div>
         </div>
-        <WishButton book={book} />
-        <ReviewButton book={{ ...book, id: book.itemId }} />
       </div>
     </section>
   );
