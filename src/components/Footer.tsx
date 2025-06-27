a7d274deb188174bf2f2439329253085e14496ea
minxng <minxng12@gmail.com>
2025-06-27 22:08:35 +0900

    [feature] footer 작업

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,15 @@
 export default function Footer() {
   return (
-    <footer className=" bg-primary py-10 static bottom-0">
-      <div className="mx-auto w-4/5 max-w-[1200px]">footer~~</div>
+    <footer className=" bg-primary py-10 mt-20 static bottom-0 text-sm text-white font-light">
+      <div className="mx-auto max-w-[1200px] text-center flex flex-col gap-4">
+        <p>알라딘 API를 이용하여 도서 정보를 제공합니다.</p>
+        <p>
+          이 프로젝트는 개인 프로젝트용으로 제작되었으며&nbsp;
+          <br className="sm:hidden" />
+          상업적 목적이 없습니다.
+        </p>
+        <p>&copy; 2025. All rights reserved.</p>
+      </div>
     </footer>
   );
 }
