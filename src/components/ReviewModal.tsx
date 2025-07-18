b4effbf932757f24b8e144e52db8784b3a6aafb5
minxng <minxng12@gmail.com>
2025-07-18 23:09:46 +0900

    [feature] 리뷰 작성창 높이 리뷰 길이만큼 설정

--- src/components/ReviewModal.tsx
+++ src/components/ReviewModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Modal from "./Modal";
 import Rating from "./Rating";
 
@@ -35,6 +35,10 @@ export default function ReviewModal({
   const [review, setReview] = useState("");
 
   useEffect(() => {
+    handleResizeHeight();
+  }, [review]);
+
+  useEffect(() => {
     if (isOpen && existingReview) {
       setReview(existingReview.review ?? "");
       setRating(existingReview.rating ?? 0);
@@ -52,25 +56,42 @@ export default function ReviewModal({
     setReview("");
     onClose();
   };
+
+  const textarea = useRef<HTMLTextAreaElement>(null);
+
+  const handleResizeHeight = () => {
+    if (textarea.current) {
+      textarea.current.style.height = "auto";
+      textarea.current.style.height = `${textarea.current.scrollHeight}px`;
+    }
+  };
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <h2 className="text-lg font-bold mb-2">{book.title}</h2>
-      <Rating value={rating} readonly={false} onChange={(v) => setRating(v)} />
-      <textarea
-        className="w-full border p-2 rounded resize-none h-24"
-        value={review}
-        onChange={(e) => setReview(e.target.value)}
-      />
-      <div className="mt-4 flex justify-end gap-2">
-        <button onClick={onClose} className="border px-4 py-2 rounded">
-          취소
-        </button>
-        <button
-          onClick={handleSave}
-          className="bg-primary-500 text-white px-4 py-2 rounded"
-        >
-          저장
-        </button>
+      <div className="flex flex-col gap-3">
+        <h2 className="text-lg font-bold">{book.title}</h2>
+        <Rating
+          value={rating}
+          readonly={false}
+          onChange={(v) => setRating(v)}
+        />
+        <textarea
+          ref={textarea}
+          rows={1}
+          className="w-full p-2 rounded max-h-96 min-h-28 border border-primary-300 text-sm focus:outline-0"
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+        />
+        <div className="mt-4 flex justify-end gap-2">
+          <button onClick={onClose} className="border px-4 py-2 rounded">
+            취소
+          </button>
+          <button
+            onClick={handleSave}
+            className="bg-primary-500 text-white px-4 py-2 rounded"
+          >
+            저장
+          </button>
+        </div>
       </div>
     </Modal>
   );
