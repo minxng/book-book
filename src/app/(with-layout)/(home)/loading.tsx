e03bef9009dd174b8fabcdb43f3f3ae78dd26bfb
minxng <minxng12@gmail.com>
2025-08-19 23:46:15 +0900

    [feature] home loading 파일 추가

---
+++ src/app/(with-layout)/(home)/loading.tsx
@@ -0,0 +1,10 @@
+import Banner from "@/components/Banner";
+
+export default function loading() {
+  return (
+    <div className="container-style my-0">
+      <Banner />
+      <div className="w-full text-center p-10">loading</div>
+    </div>
+  );
+}
