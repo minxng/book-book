df149879b135ceae2d853e7186ab42cdb2104c69
minxng <minxng12@gmail.com>
2025-08-23 20:31:19 +0900

    [fix] 카테고리 초기 선택, 새로고침시 카테고리명 정상 노출

--- src/components/SideCategorise.tsx
+++ src/components/SideCategorise.tsx
@@ -2,16 +2,30 @@
 
 import { categories } from "@/app/constant";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 export default function SideCategorise({ categoryId }: { categoryId: number }) {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const [currentCategory, setCurrentCategory] = useState("");
+
+  useEffect(() => {
+    const categoryId = searchParams.get("categoryId") || "";
+    setCurrentCategory(categoryId);
+  }, [searchParams]);
+
   const selectCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCurrentCategory(e.target.value);
     router.push(`/list/bestSeller?categoryId=${e.target.value}`);
   };
   return (
-    <div className="flex sm:block items-center">
+    <div className="flex sm:block items-center mb-4 sm:mb-0">
       <p className="font-bold text-lg sm:mb-3">분야별 베스트셀러</p>
-      <select onChange={selectCategory} className="sm:hidden">
+      <select
+        value={currentCategory}
+        onChange={selectCategory}
+        className="sm:hidden ml-2"
+      >
         <option value="">선택</option>
         {categories.map((category) => {
           return (
