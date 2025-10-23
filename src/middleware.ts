612f7ec651d8bb68356d7660edc0ad5dd29b926f
minxng <minxng12@gmail.com>
2025-10-24 06:02:28 +0900

    [feature] 로그인 상태에 따른 접근 제어 미들웨어 추가 및 로그인/로그아웃 시 페이지 리디렉션 처리

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const publicPaths = ["/_next", "/favicon.ico"];
+  if (publicPaths.some((path) => pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+
+  const token = req.cookies.get("authToken")?.value;
+  const authPages = ["/login", "/sign-up"];
+  if (token && authPages.some((path) => pathname.startsWith(path))) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!token) {
+    if (authPages.includes(pathname)) {
+      return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/:path*"],
+};
